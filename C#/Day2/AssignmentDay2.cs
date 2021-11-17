360634f59c276014f01ba922bd94ce5b39b1a86d
vipuldabhi <vipuldabhi5560@gmail.com>
2021-11-17 10:43:23 +0530

    REFS:#1801 and fixes:#1801 (Added C# Day2 Files)

---
+++ C#/Day2/AssignmentDay2.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    class AssignmentDay2
+    {
+
+
+        static void Main(string[] args)
+        {
+                       
+            Day2.Person[] person = new Day2.Person[5];
+            string firstname, lastname, email;
+            //DateTime dateofbirth;
+            
+
+            for (int i = 0; i < person.Length; i++) 
+            {
+                Console.WriteLine("Enter FirstName : ");
+                firstname = Console.ReadLine();
+                Console.WriteLine("Enter LastName : ");
+                lastname = Console.ReadLine();
+                Console.WriteLine("Enter Email Address : ");
+                email = Console.ReadLine();
+
+                Console.Write("Enter a month: ");
+                int month = int.Parse(Console.ReadLine());
+                Console.Write("Enter a day: ");
+                int day = int.Parse(Console.ReadLine());
+                Console.Write("Enter a year: ");
+                int year = int.Parse(Console.ReadLine());
+
+                DateTime dateofbirth = new DateTime(year, month, day);
+
+                person[i] = new Day2.Person(firstname, lastname, email, dateofbirth);
+
+                person[i].calculate();
+
+
+            }
+            //2020-2-22
+
+            Console.WriteLine("FirstName|\tLastName\tEmail\tDateOfBirth\tAdult\tsunsign\tchinisesign\tbirthDayOrNot\tusername\n");
+            Console.WriteLine("--------------------------------------------------------------------------------------------------------\n");
+            for (int i = 0; i < person.Length; i++)
+            {
+                Console.WriteLine($"{person[i].FirstName}\t{person[i].LastName}\t{person[i].EmailAddress}\t{person[i].DateOfBirth}\t{person[i].Adult}\t{person[i].Sunsign}\t{person[i].Chinisesign}\t{person[i].BirthDayOrNot}\t{person[i].Username}\n");
+            }
+            Console.ReadLine();
+
+
+
+        }
+
+
+
+    }
+}
---
+++ C#/Day2/PracticeDay2.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    class PracticeDay2
+    {
+    }
+
+    class Employee
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+        public DateTime StartingDate { get; set; }
+        public string PhoneNumber { get; set; }
+
+
+        public Employee(int age, string name, double salary, DateTime startingdate, string phonenumber)
+        {
+            Age = age;
+            Name = name;
+            Salary = salary;
+            StartingDate = startingdate;
+            PhoneNumber = phonenumber;
+        }
+    }
+
+    class operation
+    {
+        int a, b;
+        public void add()
+        {
+
+        }
+    }
+
+
+    class Class1
+    {
+        public int addition(int num1, int num2)
+        {
+            int result;
+            result = num1 + num2;
+            return result;
+        }
+        public int subtraction(int num1, int num2)
+        {
+            int result;
+            result = num1 - num2;
+            return result;
+        }
+        public int multiplication(int num1, int num2)
+        {
+            int result;
+            result = num1 * num2;
+            return result;
+        }
+        public int division(int num1, int num2)
+        {
+            int result;
+            result = num1 / num2;
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            //Employee dav = new Employee();
+            //dav.Age = 30;
+            //Console.WriteLine(dav.Age); 
+
+            //Employee dav = new Employee()
+            //{
+            //    Age = 30,
+            //    Salary = 50000
+            //};
+            //Console.WriteLine($"dav's Age is {dav.Age} And Salary is {dav.Salary}");
+
+            //Employee construct = new Employee(23, "john", 5000.00, new DateTime(2021, 2, 22), "9876546789");
+            //Console.WriteLine("{0}'s age is {1} salary is {2}",construct.Name,construct.Age,construct.Salary);
+
+
+            Class1 operation = new Class1();
+
+            int a, b;
+            Console.WriteLine("Enter First Number");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Second Number");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            int result = operation.addition(a, b);
+            Console.WriteLine("Result is :" + result);
+            Console.ReadLine();
+        }
+    }
+}
