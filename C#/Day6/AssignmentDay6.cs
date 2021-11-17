7ee25eee11dde01ff338d59ce908a5a0b5606ea9
vipuldabhi <vipuldabhi5560@gmail.com>
2021-11-17 10:48:51 +0530

    REFS:#1917 and fixes:#1917 (Added C# Day6 Files)

---
+++ C#/Day6/AssignmentDay6.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6
+{
+    class AssignmentDay6
+    {
+        public double calculateArea(double x, double y)
+        {
+            return x * y;
+        }
+
+        static void Main(string[] args)
+        {
+            //1.Compute area of rectangle using func delegate
+
+            AssignmentDay6 obj = new AssignmentDay6();
+
+            Console.WriteLine("Enter a :");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter b :");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+
+            Func<double, double, double> area = obj.calculateArea;
+
+            double result = area(a, b);
+            Console.WriteLine("Area Of Rectengle is :" + result);
+
+
+
+            //2.Compute add of two number using lambda expression
+
+
+            Func<int, int, int> add = (x,y) => x + y;
+
+            int z = add(2, 4);
+
+            Console.WriteLine(z);
+
+
+            Console.ReadLine();
+        }
+
+        
+
+
+    }
+}
---
+++ C#/Day6/PracticeDay6.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6
+{
+    class PracticeDay6
+    {
+        static void Main(string[] args)
+        {
+            // Declare a Func variable and assign a lambda expression to the
+            // variable. The method takes a string and converts it to uppercase.
+            Func<string, string> selector = str => str.ToUpper();
+            // Create an array of strings.
+            string[] words = { "orange", "apple", "Article", "elephant" };
+            // Query the array and select strings according to the selector method.
+            IEnumerable<String> aWords = words.Select(selector);
+            // Output the results to the console.
+            foreach (String word in aWords)
+                Console.WriteLine(word);
+            /*
+            This code example produces the following output:
+            ORANGE
+            APPLE
+            ARTICLE
+            ELEPHANT
+            */
+
+
+            //// Instantiate delegate to reference UppercaseString method
+            //ConvertMethod convertMeth = UppercaseString;
+            //string name = "Dakota";
+            //// Use delegate instance to call UppercaseString method
+            //Console.WriteLine(convertMeth(name));
+
+            //// Instantiate delegate to reference UppercaseString method
+            //Func<string, string> convertMethod = UppercaseString;
+            //string name = "Dakota";
+            //// Use delegate instance to call UppercaseString method
+            //Console.WriteLine(convertMethod(name));
+            //string UppercaseString(string inputString)
+            //{
+            //    return inputString.ToUpper();
+            //}
+            //// This code example produces the following output:
+            ////
+            ////    DAKOTA
+            
+
+
+
+        }
+        private static string UppercaseString(string inputString)
+        {
+            return inputString.ToUpper();
+        }
+    }
+
+    //event example
+
+    //public delegate string MyDel(string str);
+    //class EventProgram
+    //{
+    //    event MyDel MyEvent;
+    //    public EventProgram()
+    //    {
+    //        this.MyEvent += new MyDel(this.WelcomeUser);
+    //    }
+    //    public string WelcomeUser(string username)
+    //    {
+    //        return "Welcome " + username;
+    //    }
+    //    static void Main(string[] args)
+    //    {
+    //        EventProgram obj1 = new EventProgram();
+    //        string result = obj1.MyEvent("Tutorials Point");
+    //        Console.WriteLine(result);
+    //    }
+    //}
+}
