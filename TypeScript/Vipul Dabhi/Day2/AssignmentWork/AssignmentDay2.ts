04fdadf594fcc5f7fcce3c62b258c7ac2bbfc770
vipuldabhi <vipuldabhi5560@gmail.com>
2021-11-24 08:55:06 +0530

    REFS:#2242 and fixes:#2242 (Added TypeScript Day2 Files)

---
+++ TypeScript/Vipul Dabhi/Day2/AssignmentWork/AssignmentDay2.ts
@@ -0,0 +1,74 @@
+// There is a retail shop which need to manage the inventory, whenever some purchase 
+// is being made product quantity should be reduced, if quanity is less than 5 reorder 
+// request should be raised.
+
+// Design an interface and classes for the same.
+
+interface Person {
+    Item1Quantity : number;
+    Item2Quantity : number;
+    Item3Quantity : number;
+}
+
+class buyProduct implements Person{
+    Item1Quantity = 7;
+    Item2Quantity = 6;
+    Item3Quantity = 5;
+    resuceItemQuantity(value : number){
+        if(value === 1){
+            this.Item1Quantity = this.Item1Quantity - 1;
+            console.log("Item1 Quantity Reduced Successfully");
+            console.log(`Remening Quantity Of Item1 is ${this.Item1Quantity}`);
+            if(this.Item1Quantity < 5){
+                console.log("ReOrder Request For Item1 is Raised");
+            }
+        }
+        else if(value === 2){
+            this.Item2Quantity = this.Item2Quantity - 1;
+            console.log("Item2 Quantity Reduced Successfully");
+            console.log(`Remening Quantity Of Item2 is ${this.Item2Quantity}`);
+            if(this.Item1Quantity < 5){
+                console.log("ReOrder Request For Item2 is Raised");
+            }
+        }
+        else{
+            this.Item3Quantity = this.Item3Quantity - 1;
+            console.log("Item3 Quantity Reduced Successfully");
+            console.log(`Remening Quantity Of Item2 is ${this.Item2Quantity}`);
+
+            if(this.Item1Quantity < 5){
+                console.log("ReOrder Request For Item1 is Raised");
+            }
+        }
+    }
+}
+
+// var flag:number = 0;
+
+// while(flag === 1){
+var value:number = 2;
+var obj = new buyProduct();
+
+switch(value) { 
+    
+    case 1: { 
+        obj.resuceItemQuantity(1);
+        break; 
+    } 
+    case 2: { 
+        obj.resuceItemQuantity(2);
+       break; 
+    } 
+    case 3: { 
+        obj.resuceItemQuantity(3);
+       break; 
+    } 
+    case 4:{
+        // flag = 0;
+    }
+    default: { 
+        console.log("Invalid Choice");
+        break; 
+    } 
+ }
+// }
---
+++ TypeScript/Vipul Dabhi/Day2/PracticeExercise/PracticeDay2.ts
@@ -0,0 +1,184 @@
+
+// Tuple
+
+// var employee: [number, string] = [1, "Steve"];
+// var person: [number, string, boolean] = [1, "Steve", true];
+
+// var user: [number, string, boolean, number, string];// declare tuple variable
+// user = [1, "Steve", true, 20, "Admin"];// initialize tuple variable
+
+// Array Of Tuple
+
+// var employee: [number, string][];
+// employee = [[1, "Steve"], [2, "Bill"], [3, "Jeff"]];
+
+// Add element into tuple
+
+// var employee: [number, string] = [1, "Steve"];
+// employee.push(2, "Bill"); 
+// console.log(employee); //Output: [1, 'Steve', 2, 'Bill']
+
+// Use Array Methos
+
+// var employee: [number, string] = [1, "Steve"];
+
+// // retrieving value by index and performing an operation 
+// employee[1] = employee[1].concat(" Jobs"); 
+// console.log(employee); //Output: [1, 'Steve Jobs']
+
+
+// Enum
+
+// There are three types of enums:
+
+// Numeric enum
+// String enum
+// Heterogeneous enum
+
+// enum PrintMedia {
+//     Newspaper,
+//     Newsletter,
+//     Magazine,
+//     Book
+//   }
+//   Newspaper = 0
+//   Newsletter = 1
+//   Magazine = 2
+//   Book = 3
+
+//   enum PrintMedia {
+//     Newspaper = 1,
+//     Newsletter = 5,
+//     Magazine = 5,
+//     Book = 10
+// }
+
+// Enum as return type
+
+// enum PrintMedia {
+//     Newspaper = 1,
+//     Newsletter,
+//     Magazine,
+//     Book
+// }
+// function getMedia(mediaName: string): PrintMedia {
+//     if (  mediaName === 'Forbes' || mediaName === 'Outlook') {
+//         return PrintMedia.Magazine;
+//     }
+//  }
+// let mediaType: PrintMedia = getMedia('Forbes'); // returns Magazine
+
+
+// Union
+
+
+// function printId(id: number | string) {
+//     console.log("Your ID is: " + id);
+//   }
+//   // OK
+//   printId(101);
+//   // OK
+//   printId("202");
+//   // Error
+//   printId({ myID: 22342 });
+//   Argument of type '{ myID: number; }' is not assignable to parameter of type 'string | number'.
+//     Type '{ myID: number; }' is not assignable to type 'number'.
+  
+// Working with Union Types
+
+// function printId(id: number | string) {
+//     console.log(id.toUpperCase()); // Not Valid
+//   }
+
+// Way of using it
+
+// function printId(id: number | string) {
+//     if (typeof id === "string") {
+//       // In this branch, id is of type 'string'
+//       console.log(id.toUpperCase());
+//     } else {
+//       // Here, id is of type 'number'
+//       console.log(id);
+//     }
+//   }
+
+// function welcomePeople(x: string[] | string) {
+//     if (Array.isArray(x)) {
+//       // Here: 'x' is 'string[]'
+//       console.log("Hello, " + x.join(" and "));
+//     } else {
+//       // Here: 'x' is 'string'
+//       console.log("Welcome lone traveler " + x);
+//     }
+//   }
+
+// Interface
+
+// interface Point {
+//     x: number;
+//     y: number;
+//   }
+   
+//   function printCoord(pt: Point) {
+//     console.log("The coordinate's x value is " + pt.x);
+//     console.log("The coordinate's y value is " + pt.y);
+//   }
+   
+//   printCoord({ x: 100, y: 100 });
+
+// Classes
+
+// class Greeter {
+//     greeting: string;
+   
+//     constructor(message: string) {
+//       this.greeting = message;
+//     }
+   
+//     greet() {
+//       return "Hello, " + this.greeting;
+//     }
+//   }
+   
+//   let greeter = new Greeter("world");
+
+// //   extend
+
+//   class Animal {
+//     move(distanceInMeters: number = 0) {
+//       console.log(`Animal moved ${distanceInMeters}m.`);
+//     }
+//   }
+   
+//   class Dog extends Animal {
+//     bark() {
+//       console.log("Woof! Woof!");
+//     }
+//   }
+   
+//   const dog = new Dog();
+//   dog.bark();
+//   dog.move(10);
+//   dog.bark();
+
+// overide Method
+
+class Base {
+    greet() {
+      console.log("Hello, world!");
+    }
+  }
+   
+  class Derived extends Base {
+    greet(name?: string) {
+      if (name === undefined) {
+        super.greet();
+      } else {
+        console.log(`Hello, ${name.toUpperCase()}`);
+      }
+    }
+  }
+   
+  const d = new Derived();
+  d.greet();
+  d.greet("reader");
