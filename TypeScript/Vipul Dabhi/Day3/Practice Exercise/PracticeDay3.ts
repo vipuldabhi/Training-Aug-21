d147e152d85c54d79a429e2f3b8f3c5365602292
vipuldabhi <vipuldabhi5560@gmail.com>
2021-11-25 09:39:04 +0530

    changes in Day3 Practice file

--- TypeScript/Vipul Dabhi/Day3/Practice Exercise/PracticeDay3.ts
+++ TypeScript/Vipul Dabhi/Day3/Practice Exercise/PracticeDay3.ts
@@ -57,3 +57,22 @@
 
 // module
 
+// export let age : number = 20;
+// export class Employee {
+//     empCode: number;
+//     empName: string;
+//     constructor(name: string, code: number) {
+//         this.empName = name;
+//         this.empCode = code;
+//     }
+//     displayEmployee() {
+//         console.log ("Employee Code: " + this.empCode + ", Employee Name: " + this.empName );
+//     }
+// }
+// let companyName:string = "XYZ";
+
+// import
+
+// import { Employee } from "./Employee";
+// let empObj = new Employee("Steve Jobs", 1);
+// empObj.displayEmployee(); //Output: Employee Code: 1, Employee Name: Steve Jobs  
